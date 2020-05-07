f924359a4c1ad2f1f1d85d45cf120abfca9ba29a
Jędrzej Krawczyk <siewierjedr@gmail.com>
2020-05-07 15:45:48 +0200

    Init project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+    
+
+render() {
+   return (
+        <div className="weather-widget">
+            <div className="sidebar">
+
+            </div>
+            <div className="carousel content">
+                <div className="left-coursor">
+                </div>
+
+                <div className="content">
+
+                </div>
+
+                <div className="right-coursor">
+                </div>
+
+            </div>
+        </div>
+    );
+}
+ 
+}
+
+export default App;
