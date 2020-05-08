35f8bc3ed1b15412108cdd4f46bcaf17b9e44a23
Jędrzej Krawczyk <siewierjedr@gmail.com>
2020-05-08 09:38:10 +0200

    create getGeolocation plugin

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
 import React from 'react';
-import './App.css';
-import Sidebar from './components/Sidebar'
 import axios from 'axios';
 
+import './App.css';
+
+import { getUserGeolocation } from './plugins/getGeolocation'
+
+import Sidebar from './components/Sidebar'
+
+
 class App extends React.Component {
-    getUserGeolocation = () => {
-        return new Promise((resolve, reject) => {
-            const options = {
-                enableHighAccuracy: true,
-                timeout: 5000,
-                maximumAge: 0
-            };
-
-            function error() {
-                reject('Unable to retrieve your location');
-            }
-
-            function success(position) {
-                const lat = position.coords.latitude;
-                const lon = position.coords.longitude;
-
-                resolve({ lat, lon });
-            }
-
-            navigator.geolocation.getCurrentPosition(success, error, options);
-        });
-    };
-    async componentDidMount() {
-        // try {
-        //     const position = await this.getUserGeolocation();
-        //     const data = await axios.get(
-        //         `http://api.openweathermap.org/data/2.5/forecast?lat=${position.lat}&lon=${position.lon}&appid=f98c38ada5226151bb36847c5dd668b8`
-        //     );
-        //     console.log(data);
-        // } catch (err) {
-        //     console.error(err);
-        // }
-    }
+    // async componentDidMount() {
+    // //     try {
+    // //         const position = await getUserGeolocation();
+    // //         const data = await axios.get(
+    // //             `http://api.openweathermap.org/data/2.5/forecast?lat=${position.lat}&lon=${position.lon}&appid=f98c38ada5226151bb36847c5dd668b8`
+    // //         );
+    // //         console.log(data);
+    // //     } catch (err) {
+    // //         console.error(err);
+    // //     }
+    // // }
 
     render() {
 
