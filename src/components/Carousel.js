beb7ca73a4c005276a0dab983c9db5fd8a82e84d
Jędrzej Krawczyk <siewierjedr@gmail.com>
2020-05-13 16:38:57 +0200

    Add scrollbar, dragable funcinality and switchers for carousel-content

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,13 +6,24 @@ import { ReactComponent as Arrow } from '../assets/media/icons/arrow.svg';
 class Carousel extends React.Component {
 
     state = {
-        xpoint: null,
+        xpoint: 0,
+        xstart: 0,
+        xend: 0,
+        contentOffsetLeft: 0,
+        barOffsetLeft: 0,
     }
 
     startDrag = (event) => {
+        const bar = document.querySelector('.carousel .scrollbar .bar')
         const scrollbar = document.querySelector('.carousel .scrollbar')
         const dragRegister = document.querySelector('.carousel .dragRegister')
-        if(event.target.className === 'dragRegister') {
+        const carousel = document.querySelector('.carousel')
+        const carouselContentChild = document.querySelector('.carousel-content *:first-child')
+
+        this.setState({ xstart: event.clientX - carousel.offsetLeft, contentOffsetLeft: carouselContentChild.offsetLeft, barOffsetLeft: bar.offsetLeft })
+
+        if (event.target.className === 'dragRegister') {
+            console.log(this.state.xstart, event.clientX)
             dragRegister.addEventListener('mousemove', this.contentManager, false)
         } else {
             scrollbar.addEventListener('mousemove', this.scrollBarManager, false)
@@ -20,86 +31,115 @@ class Carousel extends React.Component {
     }
 
     endDrag = (event) => {
+        const carousel = document.querySelector('.carousel')
         const scrollbar = document.querySelector('.carousel .scrollbar')
         const bar = document.querySelector('.carousel .scrollbar .bar')
         const carouselContentChild = document.querySelector('.carousel-content *:first-child')
         const dragRegister = document.querySelector('.carousel .dragRegister')
 
-        if(event.target.className === 'dragRegister') {
-            dragRegister.removeEventListener('mousemove', this.contentManager, false)
-        } else {
-            scrollbar.removeEventListener('mousemove', this.scrollBarManager, false)
+        const scrollBarEndpoint = scrollbar.offsetWidth - bar.offsetWidth
+        const carouselContentEndpoint = -carouselContentChild.offsetWidth + carousel.offsetWidth
+
+        dragRegister.removeEventListener('mousemove', this.contentManager, false)
+        scrollbar.removeEventListener('mousemove', this.scrollBarManager, false)
+
+        if (bar.offsetLeft < 0 || carouselContentChild.offsetLeft > 0) {
+            bar.style.left = `1px`
+            carouselContentChild.style.left = `-1px`
         }
 
-        bar.style.left = `${this.interpolation(bar.offsetLeft, scrollbar.offsetWidth)}px`
-        carouselContentChild.style.left = `${this.interpolation(carouselContentChild.offsetLeft, carouselContentChild.offsetWidth)}px`
+
+        if (bar.offsetLeft > scrollBarEndpoint || carouselContentChild.offsetLeft < carouselContentEndpoint) {
+            bar.style.left = `${carousel.offsetWidth - bar.offsetWidth}px`
+            carouselContentChild.style.left = `${-carouselContentChild.offsetWidth + carousel.offsetWidth}px`
+        }
+
+
+        bar.style.left = `${this.widthInterpolation(bar.offsetLeft, scrollbar.offsetWidth)}px`
+        carouselContentChild.style.left = `${this.widthInterpolation(carouselContentChild.offsetLeft, carouselContentChild.offsetWidth)}px`
     }
 
-    interpolation(value, containerWidth) {
+    widthInterpolation(value, containerWidth) {
         const elementDeltaWidth = containerWidth / this.props.elements
 
-        return (Math.round(value / elementDeltaWidth) * elementDeltaWidth).toFixed(0)
+        return (Math.round(value / elementDeltaWidth) * elementDeltaWidth).toFixed(2)
 
     }
 
     scrollBarManager = (event) => {
 
         const carousel = document.querySelector('.carousel')
-        const scrollbar = document.querySelector('.carousel .scrollbar')
         const bar = document.querySelector('.carousel .scrollbar .bar')
         const carouselContentChild = document.querySelector('.carousel-content *:first-child')
 
-        const moveX = event.clientX - carousel.offsetLeft - bar.offsetWidth / 2;
-        const endpoint = carousel.offsetWidth - bar.offsetWidth
+        const moveX = event.clientX - carousel.offsetLeft;
 
-        if (moveX < 0) {
-            bar.style.left = `0px`
-            carouselContentChild.style.left = `0px`
-        }
+        const deltaMove = moveX - this.state.xstart
+        const scrollRange = carousel.offsetWidth - bar.offsetWidth
+        const carouselRange = carouselContentChild.offsetWidth - carousel.offsetWidth
 
-        else if (moveX <= endpoint) {
-            this.setState({ xpoint: moveX })
-            bar.style.left = `${this.state.xpoint}px`
-            carouselContentChild.style.left = `${-(carouselContentChild.offsetWidth - carousel.offsetWidth/2) * (this.state.xpoint / carousel.offsetWidth)}px`
-        }
+        const deltaMoveCarousel = (carouselRange / scrollRange) * deltaMove
 
-        else {
-            this.setState({ xpoint: endpoint })
-            bar.style.left = `${endpoint}px`
-            carouselContentChild.style.left = `${-(carouselContentChild.offsetWidth)}`
-        }
+        this.setState({ xpoint: moveX })
+
+        bar.style.left = `${moveX - bar.offsetWidth / 2}px`
+        carouselContentChild.style.left = `${(this.state.contentOffsetLeft - deltaMoveCarousel)}px`
+
     }
 
     contentManager = (event) => {
 
         const carousel = document.querySelector('.carousel')
-        const scrollbar = document.querySelector('.carousel .scrollbar')
         const bar = document.querySelector('.carousel .scrollbar .bar')
         const carouselContentChild = document.querySelector('.carousel-content *:first-child')
 
-        const moveX = event.clientX - carousel.offsetLeft - bar.offsetWidth / 2;
-        const endpoint = carousel.offsetWidth - bar.offsetWidth
+        const moveX = event.clientX - carousel.offsetLeft;
 
-        if (moveX < 0) {
-            bar.style.left = `0px`
-            carouselContentChild.style.left = `0px`
-        }
+        const deltaMove = moveX - this.state.xstart
+        const scrollRange = carousel.offsetWidth - bar.offsetWidth
+        const carouselRange = carouselContentChild.offsetWidth - carousel.offsetWidth
 
-        else if (moveX <= endpoint) {
-            this.setState({ xpoint: moveX })
-            bar.style.left = `${this.state.xpoint}px`
-            carouselContentChild.style.left = `${-(carouselContentChild.offsetWidth - carousel.offsetWidth) * (this.state.xpoint / scrollbar.offsetWidth)}px`
-        }
+        const deltaMoveScrollbar = (scrollRange / carouselRange) * deltaMove
+        console.log(deltaMoveScrollbar)
+        this.setState({ xpoint: moveX })
+        bar.style.left = `${(this.state.barOffsetLeft - deltaMoveScrollbar)}px`
+        carouselContentChild.style.left = `${this.state.contentOffsetLeft + deltaMove}px`
 
-        else {
-            this.setState({ xpoint: endpoint })
 
-            bar.style.left = `${endpoint}px`
-            carouselContentChild.style.left = `${-(carouselContentChild.offsetWidth)}`
-        }
     }
 
+    moveRightHandler(event) {
+        const carouselContentChild = document.querySelector('.carousel-content *:first-child')
+        const scrollbar = document.querySelector('.carousel .scrollbar')
+        const bar = document.querySelector('.carousel .scrollbar .bar')
 
+        const deltaCarouselContentWidth = carouselContentChild.offsetWidth / this.props.elements
+
+        const scrollRange = scrollbar.offsetWidth - bar.offsetWidth
+        const carouselRange = carouselContentChild.offsetWidth - scrollbar.offsetWidth
+
+        const deltaMoveScrollbar = (scrollRange / carouselRange) * deltaCarouselContentWidth
+
+        bar.style.left = `${bar.offsetLeft + deltaMoveScrollbar}px`
+        carouselContentChild.style.left = `${carouselContentChild.offsetLeft - deltaCarouselContentWidth}px`
+    }
+
+    moveLeftHandler(event) {
+        const carouselContentChild = document.querySelector('.carousel-content *:first-child')
+        const scrollbar = document.querySelector('.carousel .scrollbar')
+        const bar = document.querySelector('.carousel .scrollbar .bar')
+
+        const deltaCarouselContentWidth = carouselContentChild.offsetWidth / this.props.elements
+
+        const scrollRange = scrollbar.offsetWidth - bar.offsetWidth
+        const carouselRange = carouselContentChild.offsetWidth - scrollbar.offsetWidth
+
+        const deltaMoveScrollbar = (scrollRange / carouselRange) * deltaCarouselContentWidth
+
+        
+        bar.style.left = `${bar.offsetLeft - deltaMoveScrollbar}px`
+        carouselContentChild.style.left = `${(carouselContentChild.offsetLeft) + deltaCarouselContentWidth}px`
+    }
 
     componentDidMount() {
 
@@ -125,15 +165,14 @@ class Carousel extends React.Component {
 
         return (
             <div className="carousel col-11">
-                <div className="left-coursor"><Arrow /></div>
-
+                <div className="dragRegister" onMouseUp={event => this.endDrag(event)} onMouseDown={event => this.startDrag(event)} ></div>
+                <div className="left-coursor" onClick={event => this.moveLeftHandler(event)}><Arrow /></div>
                 <div className="carousel-content"  >{this.props.children}</div>
-
-                <div className="right-coursor"><Arrow /></div>
+                <div className="right-coursor" onClick={event => this.moveRightHandler(event)}><Arrow /></div>
                 <div className="scrollbar" onMouseUp={event => this.endDrag(event)} onMouseDown={event => this.startDrag(event)} >
                     <div className="bar"></div>
                 </div>
-                <div className="dragRegister"  onMouseUp={event => this.endDrag(event)} onMouseDown={event => this.startDrag(event)} ></div>
+
             </div>
         );
     }
