4ee1651bc4d5709e7caac2b5324404cac58023ed
Jędrzej Krawczyk <siewierjedr@gmail.com>
2020-05-14 10:29:14 +0200

    Updates

---
+++ precache-manifest.d50a00b42e2a68c92440d6e51cb5fcf4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea26e9b3e257bfae1edfb2fe42d829a4",
+    "url": "/WeatherWidget/index.html"
+  },
+  {
+    "revision": "af9dfa3fe7938f639887",
+    "url": "/WeatherWidget/static/css/2.e50de653.chunk.css"
+  },
+  {
+    "revision": "b3535e4d8dee0ca1c21c",
+    "url": "/WeatherWidget/static/css/main.5ef81c86.chunk.css"
+  },
+  {
+    "revision": "af9dfa3fe7938f639887",
+    "url": "/WeatherWidget/static/js/2.6e285daf.chunk.js"
+  },
+  {
+    "revision": "dd5958a82507b7f6fca3ce3dc2fb279b",
+    "url": "/WeatherWidget/static/js/2.6e285daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3535e4d8dee0ca1c21c",
+    "url": "/WeatherWidget/static/js/main.4bd2f628.chunk.js"
+  },
+  {
+    "revision": "4ecebc001bdce3688395",
+    "url": "/WeatherWidget/static/js/runtime-main.02dbf54d.js"
+  },
+  {
+    "revision": "21e3b6d164c88738eb0dd81f285c7688",
+    "url": "/WeatherWidget/static/media/arrow.21e3b6d1.svg"
+  },
+  {
+    "revision": "32d74b5ad782e0748a8174a92c8e187e",
+    "url": "/WeatherWidget/static/media/cursor.32d74b5a.svg"
+  }
+]);
